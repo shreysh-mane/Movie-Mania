66726f7ecd0f50e6aed1bb8b9e936213ccc0b03f
shreysh-mane <shreysh.mane12@gmail.com>
2021-08-18 21:00:46 +0530

    debug

---
+++ src/Components/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    horror: {
+      h1: {
+        name: "Annabelle Comes Home ",
+        link: "https://www.amazon.com/Annabelle-Comes-Home-McKenna-Grace/dp/B07VNBVXVB/ref=sr_1_1?dchild=1&keywords=annabel+comes+home&qid=1629297288&s=instant-video&sr=1-1",
+        img: "https://m.media-amazon.com/images/I/81L6jWlP6IL._AC_UY218_.jpg",
+        description: "McKenna Grace , Madison Iseman , Katie Sarife ",
+      },
+      h2: {
+        name: "Annabelle Creation",
+        link: "https://www.amazon.com/Annabelle-Creation-Stephanie-Sigman/dp/B0747R7YWZ/ref=sr_1_2?dchild=1&keywords=annbel&qid=1629284538&s=instant-video&sr=1-2",
+        img: "https://m.media-amazon.com/images/I/817lK8mkbjL._AC_UY218_.jpg",
+        description: "Stephanie Sigman , Talitha Bateman , Anthony LaPaglia ",
+      },
+      h3: {
+        name: "Annabelle",
+        link: "https://www.amazon.com/Annabelle-Wallis/dp/B00O4UQ7JU/ref=sr_1_3?dchild=1&keywords=annbel&qid=1629284538&s=instant-video&sr=1-3",
+        img: "https://m.media-amazon.com/images/I/812Py0hw47L._AC_UY218_.jpg",
+        description: "Annabelle Wallis , Ward Horton and Alfre Woodard ",
+      },
+      h4: {
+        name: "IT Chapter Two",
+        link: "https://www.amazon.com/Chapter-Two-James-McAvoy/dp/B07XD9HWCQ/ref=sr_1_4?dchild=1&keywords=annbel&qid=1629284538&s=instant-video&sr=1-4",
+        img: "https://m.media-amazon.com/images/I/91Qd+PHkr9L._AC_UY218_.jpg",
+        description: "James McAvoy , Jessica Chastain , Bill Hader ",
+      },
+    },
+    Action: {
+      a1: {
+        name: "Shershaah",
+        link: "https://www.amazon.com/Shershaah-Sidharth-Malhotra/dp/B09C38LX3P/ref=sr_1_1?dchild=1&keywords=action&qid=1629293175&s=instant-video&sr=1-1",
+        img: "https://m.media-amazon.com/images/I/91UQ2ZgvYSL._AC_UY218_.jpg",
+        description: "Sidharth Malhotra , Kiara Advani , Shiv Panditt ",
+      },
+      a2: {
+        name: "Honest Thief",
+        link: "https://www.amazon.com/Honest-Thief-Liam-Neeson/dp/B08P7WZM2S/ref=sr_1_2?dchild=1&keywords=action&qid=1629293175&s=instant-video&sr=1-2",
+        img: "https://m.media-amazon.com/images/I/913KUc0uuAL._AC_UY218_.jpg",
+        description:
+          " Liam Neeson , Kate Walsh , Robert Patrick and Anthony Ramos ",
+      },
+    },
+  },
+];
+// console.log(typeof data);
